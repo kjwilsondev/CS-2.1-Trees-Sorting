ef172e6c112f75d909fb837ae6300c2adf45c09b
KJ Wilson <yourboykjdev@gmail.com>
2019-10-23 15:28:06 -0700

    implemented bubble sort :umbrella:

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -4,9 +4,11 @@ import os
 import time
 
 def is_sorted(items):
-    """Return a boolean indicating whether given items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    """
+    Return a boolean indicating whether given items are in sorted order.
+    Running time: O(n) - iterates through all elements in array  worst case
+    Memory usage: O(1) - no new memory used
+    """
     for i, item in enumerate(items):
         if i == len(items) - 1:
             return True
@@ -15,12 +17,29 @@ def is_sorted(items):
 
 
 def bubble_sort(items):
-    """Sort given items by swapping adjacent items that are out of order, and
+    """
+    Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Swap adjacent items that are out of order
+    Running time: O(n^2) - for loop within a while loop
+    Memory usage: O(logn) - new slice made for every for loop iteration, every slice gets smaller
+    """
+    size = len(arr)
+    print(f"size: {size}")
+    while size > 0:
+        # i for index
+        # e for array[element]
+        for i in range(0, size):
+            # print(i, arr[i])
+            # print(arr)
+            # check if last in list
+            if i == size - 1:
+                break
+            # checks if neighbor is bigger
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+        # biggest element has been moved to end
+        size -= 1
+
 
 
 def selection_sort(items):
@@ -48,11 +67,17 @@ if __name__ == "__main__":
     print(f"array: {arr}")
     print(f"is sorted: {is_sorted(arr)}")
     if not is_sorted(arr):
-        print(f"Method: Bubble Sort")
-        print(f"Sorting...")
-        # arr = bubble_sort(arr)
-        print(f"Done")
-        print(f"array: {arr}")
+        print("b for bubble sort")
+        print("s for selection sort")
+        method = input("method: ")
+        if method == "b":
+            print(f"Method: Bubble Sort")
+            print(f"Sorting...")
+            bubble_sort(arr)
+            print(f"Done")
+            print(f"array: {arr}")
+        else:
+            print("method not found")
     else:
         print(f"array is already sorted")
 
